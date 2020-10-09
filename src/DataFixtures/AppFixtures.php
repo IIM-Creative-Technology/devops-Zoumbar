1b3336c2f0d34e1007e4842fdb015c5e5bd63ce6
Esteban <Esteban@isee-u.fr>
2020-10-09 23:22:00 +0200

    Few Changes

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,29 +4,27 @@ namespace App\DataFixtures;
 
 use App\Entity\Post;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-
+        // $product = new Product();
+        // $manager->persist($product);
         $faker = Factory::create('fr_FR');
-        
-        for($i = 0 ; $i < 10 ; $i++ ){
-        $post = new Post();
-        $post -> setTitle($faker->sentence($nbWords = 4, $variableNbWords = true))
-            ->setContent($faker->sentence($nbWords = 15, $variableNbWords = true))
-            ->setAuthor($faker->name())
-            ->setCreatedAt($faker->dateTimeBetween('-6 months'));
-         
-        $manager->persist($post);
+
+        for ($i = 0; $i < 10; $i++) {
+            $post = new Post();
+            $post->setTitle($faker->sentence($nbWords = 2, $variableNbWords = true))
+                ->setContent($faker->sentence($nbWords = 10, $variableNbWords = true))
+                ->setAuthor($faker->name())
+                ->setCreatedAt($faker->dateTimeBetween('-6 months'));
+
+            $manager->persist($post);
         }
-        
+
         $manager->flush();
     }
-
-
-   
 }
